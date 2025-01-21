16ec665ff0fdafa001ef64f2f66376d569486fe5
Shinjirou <htooagwai9@gmail.com>
2025-01-21 15:16:08 +0630

    Add Babel for browsers support

--- webpack.common.js
+++ webpack.common.js
@@ -27,6 +27,18 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg"fig)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+            cacheDirectory: true,
+          },
+        },
+      },
     ],
   },
 };
